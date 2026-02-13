a1310fa100095856fa3546bb88c3ebb0be20b005
Sowrabh Shettigar💻 <sowrabhshettigar114@gmail.com>
2026-02-13 11:59:33 +0530

    Time: 15 ms (20.52%) | Memory: 46.4 MB (32.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,18 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
 
-        StringBuilder str = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            char letter = Character.toLowerCase(s.charAt(i));
-            if ((letter >= 'a' && letter <= 'z') || (letter >= '0' && letter <= '9')) {
-                str.append(letter);
-            }
-        }
+        String str = s.trim().replaceAll("[^A-Za-z0-9]", "").toLowerCase();
 
         int left = 0;
         int right = str.length() - 1;
-
         while (left < right) {
             if (str.charAt(left) != str.charAt(right)) {
                 return false;
@@ -20,7 +12,6 @@ class Solution {
             left++;
             right--;
         }
-
         return true;
     }
 }
