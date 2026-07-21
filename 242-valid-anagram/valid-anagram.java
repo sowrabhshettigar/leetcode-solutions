8d0774383f4727fcd1cc22b7f6ee8ec462b1f55c
Sowrabh Shettigar💻 <sowrabhshettigar114@gmail.com>
2026-07-21 16:17:00 +0530

    Time: 7 ms (32.16%) | Memory: 44.3 MB (82.79%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int count[] = new int[26];
-
-        if (s.length() != t.length())
+        char count[] = new char[26];
+        if (s.length() != t.length()) {
             return false;
+        }
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-        for (int num : count) {
-            if (num != 0) {
+        for (int c : count) {
+            if (c != 0) {
                 return false;
             }
         }
